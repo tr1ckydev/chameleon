e2028eddd861cc55848f4efecaea58bb6c473174
Andreas Stocker <xflow-systems@proton.me>
2024-02-25 22:25:12 +0100

    fix build.zig - source_file -> root_source_file

--- build.zig
+++ build.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("chameleon", .{
-        .source_file = .{ .path = "src/chameleon.zig" },
+        .root_source_file = .{ .path = "src/chameleon.zig" },
     });
 }
